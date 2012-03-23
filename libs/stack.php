57626522b1e67ff2fc02942151c2fd463e41b05e
Cauan Cabral <cauan@radig.com.br>
2012-03-23 11:12:14 -0400

    Melhoria na documentação da EvalMath
    Inclusão de novos casos de teste
    Utilização de arredondamento em algumas operações com ponto flutuante - nenhum teste antigo foi quebrado.
    Inclusão de suporte a log na base 10
    Inclusão de suporte a números localizados
    Inclusão de um método para limpar variáveis e funções

--- libs/stack.php
+++ libs/stack.php
@@ -1,24 +1,29 @@
 <?php
+/**
+ * Implementação de uma pilha simples.
+ *
+ *
+ */
 class Stack
 {
 	/**
 	 * Array utilizado como container
 	 * da pilha.
-	 * 
+	 *
 	 * @var array
 	 */
 	private $stack = array();
-	
+
 	/**
 	 * Número de elementos na pilha.
-	 * 
+	 *
 	 * @var int
 	 */
 	private $elements = 0;
 
 	/**
 	 * Inseri um elemento no topo da pilha.
-	 * 
+	 *
 	 * @param mixed $val
 	 */
 	public function push($val)
@@ -29,7 +34,7 @@ class Stack
 
 	/**
 	 * Remove e retorna o elemento no topo da pilha.
-	 * 
+	 *
 	 * @return mixed Elemento da pilha, caso ela não esteja vazia
 	 * ou null, caso esteja.
 	 */
@@ -40,16 +45,16 @@ class Stack
 			$this->elements--;
 			return array_pop($this->stack);
 		}
-		
+
 		return null;
 	}
 
 	/**
 	 * Retorna o n-ésimo elemento na pilha, em relação ao topo.
 	 * Pode ser utilizado com um lookahead.
-	 * 
+	 *
 	 * @param int $n
-	 * 
+	 *
 	 * @return mixed $this->stack[nth] if it exist and null otherwise
 	 */
 	public function nth($n = 1)
@@ -61,13 +66,13 @@ class Stack
 				return $this->stack[$this->elements - $n];
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	/**
 	 * Retorna a quantidade de elementos na pilha.
-	 * 
+	 *
 	 * @return int $elements
 	 */
 	public function size()
--- tests/cases/libs/eval_math.test.php
+++ tests/cases/libs/eval_math.test.php
@@ -6,212 +6,276 @@ class EvalMathTest extends CakeTestCase
 	public function testDefaultConfig()
 	{
 		$Eval = new EvalMath();
-		
+
 		$expected = array();
-		
+
 		$this->assertFalse($Eval->suppress_errors);
 		$this->assertEqual($Eval->funcs(), $expected);
 		$this->assertEqual($Eval->vars(), $expected);
 		$this->assertIdentical($Eval->getLastError(), null);
-		
+
 		unset($Eval);
 	}
-	
+
 	public function testNonDefaultConfig()
 	{
 		$Eval = new EvalMath(1);
-		
+
 		$result = $Eval->evaluate(1);
 		$this->assertIdentical('1.0', $result);
-		
+
 		$result = $Eval->evaluate(-1);
 		$this->assertIdentical('-1.0', $result);
-		
+
 		$result = $Eval->evaluate(1, 2);
 		$this->assertIdentical('1.00', $result);
-		
+
 		$result = $Eval->evaluate(-1, 4);
 		$this->assertIdentical('-1.0000', $result);
-	
+
 		unset($Eval);
 	}
-	
+
 	public function testBasicsWithDefault()
 	{
 		$Eval = new EvalMath();
-		
+
 		$expected = array();
-		
+
 		$result = $Eval->evaluate(1);
 		$this->assertIdentical('1.000', $result);
-		
+
 		$result = $Eval->evaluate(-1);
 		$this->assertIdentical('-1.000', $result);
-		
+
 		$result = $Eval->evaluate('1 + 2');
 		$this->assertIdentical('3.000', $result);
-		
+
 		$result = $Eval->evaluate('3 * 2.5');
 		$this->assertIdentical('7.500', $result);
-		
+
 		$result = $Eval->evaluate('5 / 2.5');
 		$this->assertIdentical('2.000', $result);
-		
+
 		unset($Eval);
 	}
-	
+
 	public function testBasicsWithNonDefault()
 	{
 		$Eval = new EvalMath(1);
-	
+
 		$result = $Eval->evaluate('1.54 + 1.36', 0);
 		$this->assertIdentical('2', $result);
-		
+
 		$result = $Eval->evaluate('1.54 + 1.36', 1);
 		$this->assertIdentical('2.9', $result);
-		
+
 		$result = $Eval->evaluate('1.54 + 1.36', 2);
 		$this->assertIdentical('2.90', $result);
-	
+
 		unset($Eval);
 	}
-	
+
 	public function testEvaluateWithVar()
 	{
 		$Eval = new EvalMath();
-		
+
 		$expected = array();
-		
+
 		$Eval->evaluate('a = 2');
 		$this->assertIdentical($Eval->vars(), array('a' => '2.000'));
-		
+
 		$result = $Eval->evaluate('a');
 		$this->assertIdentical('2.000', $result);
-		
+
 		$result = $Eval->evaluate('a + 2');
 		$this->assertIdentical('4.000', $result);
-		
+
 		$result = $Eval->evaluate('a - 3');
 		$this->assertIdentical('-1.000', $result);
-		
+
 		$result = $Eval->evaluate('a * 3');
 		$this->assertIdentical('6.000', $result);
-		
+
 		$result = $Eval->evaluate('a / 2');
 		$this->assertIdentical('1.000', $result);
-		
+
 		unset($Eval);
 	}
-	
+
+	public function testClear()
+	{
+		$Eval = new EvalMath();
+
+		$Eval->evaluate('a = 2');
+		$Eval->evaluate('f(a) = 2a');
+
+		$this->assertIdentical(array('a' => '2.000'), $Eval->vars());
+		$Eval->clear('vars');
+		$this->assertIdentical(array(), $Eval->vars());
+
+		$this->assertIdentical(array('f(a)'), $Eval->funcs());
+		$Eval->clear('funcs');
+		$this->assertIdentical(array(), $Eval->funcs());
+
+		$Eval->evaluate('a = 2');
+		$Eval->evaluate('f(a) = 2a');
+		$Eval->clear();
+		$this->assertIdentical(array(), $Eval->funcs());
+		$this->assertIdentical(array(), $Eval->vars());
+	}
+
 	public function testEvaluateWithManyVars()
 	{
 		$Eval = new EvalMath();
-	
+
 		$expected = array();
-	
+
 		$Eval->evaluate('a = 2');
 		$Eval->evaluate('b = 3;');
 		$this->assertIdentical($Eval->vars(), array('a' => '2.000', 'b' => '3.000'));
-	
+
 		$result = $Eval->evaluate('a');
 		$this->assertIdentical('2.000', $result);
-		
+
 		$result = $Eval->evaluate('b');
 		$this->assertIdentical('3.000', $result);
-	
+
 		$result = $Eval->evaluate('a + b');
 		$this->assertIdentical('5.000', $result);
-	
+
 		$result = $Eval->evaluate('a - b');
 		$this->assertIdentical('-1.000', $result);
-	
+
 		$result = $Eval->evaluate('a * b');
 		$this->assertIdentical('6.000', $result);
-		
+
 		$result = $Eval->evaluate('a ^ b');
 		$this->assertIdentical('8.000', $result);
-	
+
 		$result = $Eval->evaluate('a / b');
 		$this->assertIdentical('0.666', $result);
-		
+
 		$Eval->evaluate('radig = 10');
-		
+
 		$result = $Eval->evaluate('radig * a');
 		$this->assertIdentical('20.000', $result);
-		
+
 		$Eval->evaluate('c = 15');
 		$Eval->evaluate('d = 2');
-		
+
 		$result = $Eval->evaluate('(c/10)*10^(1+d)');
 		$this->assertIdentical('1500.000', $result);
-	
+
 		unset($Eval);
 	}
-	
+
+	public function testEvaluateWithManyVarsAndHighPrecision()
+	{
+		$Eval = new EvalMath(4);
+		$Eval->evaluate('a = 125.8116');
+		$Eval->evaluate('b = 125.7599');
+		$Eval->evaluate('c = 250');
+
+		$result = $Eval->evaluate('((a-b)*10^6)/c');
+		$this->assertIdentical(206.8, (float)$result);
+		$this->assertIdentical('206.8000', $result);
+
+		$Eval->clear();
+		$Eval->evaluate('a = 25.2524');
+		$Eval->evaluate('b = 25.1028');
+		$Eval->evaluate('c = 25');
+
+		$result = $Eval->evaluate('((a-b)*10^6)/c');
+		$this->assertIdentical(5984.0, (float)$result);
+		$this->assertIdentical('5984.0000', $result);
+
+		$Eval->clear();
+		$Eval->evaluate('a = 25.1917');
+		$Eval->evaluate('b = 25.1028');
+		$Eval->evaluate('c = 25');
+
+		$result = $Eval->evaluate('((a-b)*10^6)/c');
+		$this->assertIdentical(3556.0, (float)$result);
+		$this->assertIdentical('3556.0000', $result);
+	}
+
 	public function testEvaluateWithFunction()
 	{
 		$Eval = new EvalMath();
-		
+
 		$Eval->evaluate('f(x,y) = x^2 + y^2 - 2x*y + 1');
 		$result = $Eval->evaluate('3*f(1,3);');
 		$this->assertIdentical('15.000', $result);
-		
+
 		$result = $Eval->funcs();
 		$this->assertIdentical(array('f(x,y)'), $result);
-		
+
 		$result = $Eval->evaluate('sin(pi/2)');
+		$this->assertIdentical('1.000', $result);
+
+		$result = $Eval->evaluate('log10(1)');
 		$this->assertIdentical('0.000', $result);
-		
+
+		$Eval->clear();
+		$Eval->evaluate('f(x) = sqrt(x)');
+		$result = $Eval->evaluate('f(4)');
+		$this->assertIdentical('2.000', $result);
+
+		$Eval->evaluate('g(x) = sqrt(2x) + ln(e)');
+		$result = $Eval->evaluate('g(2)');
+		$this->assertIdentical('3.000', $result);
+
 		unset($Eval);
 	}
-	
+
 	public function testIllegalOperations()
 	{
 		$Eval = new EvalMath();
-		
+
 		$result = $Eval->evaluate('1 / 0');
 		$this->assertEqual($Eval->getLastError(), 'Division by zero');
 		$this->assertIdentical(false, $result);
-		
+
 		$result = $Eval->evaluate('a');
 		$this->assertEqual($Eval->getLastError(), 'Undefined variable \'a\'');
 		$this->assertIdentical(false, $result);
-		
+
 		$result = $Eval->evaluate('_2');
 		$this->assertEqual($Eval->getLastError(), 'Illegal character \'_\'');
 		$this->assertIdentical(false, $result);
-		
+
 		$result = $Eval->evaluate('e = 1');
 		$this->assertEqual($Eval->getLastError(), 'Cannot assign to constant \'e\'');
 		$this->assertIdentical(false, $result);
-		
+
 		$result = $Eval->evaluate('sin(x) = 2*x');
 		$this->assertEqual($Eval->getLastError(), 'Cannot redefine built-in function \'sin()\'');
-		
+
 		$result = $Eval->evaluate('a(1,2');
 		$this->assertEqual($Eval->getLastError(), 'Unexpected \',\'');
-		
+
 		unset($Eval);
 	}
-	
+
 	public function testInvalidExpressions()
 	{
 		$Eval = new EvalMath();
-		
+
 		$Eval->suppress_errors = true;
-		
+
 		$result = $Eval->evaluate('()');
 		$this->assertEqual($Eval->getLastError(), 'Unexpected \')\'');
-		
+
 		$result = $Eval->evaluate('1)');
 		$this->assertEqual($Eval->getLastError(), 'Unexpected \')\'');
-		
+
 		$result = $Eval->evaluate('((');
 		$this->assertEqual($Eval->getLastError(), 'Expecting \')\'');
-		
+
 		$result = $Eval->evaluate('2+3+');
 		$this->assertEqual($Eval->getLastError(), 'Operator \'+\' lacks operand');
-		
+
 		unset($Eval);
 	}
 }
